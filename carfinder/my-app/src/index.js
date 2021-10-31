038f6c9376f48ea168a29d6706ab19b51353c271
KxPlayer <69983255+KxPlayer@users.noreply.github.com>
2021-10-30 19:36:15 -0700

    Updated API call, connected Spring and React, and added extra filtering
    Go to the carfinder directory and use mvnw spring-boot:run in order to run react and spring-boot at the same time.
    On localhost:8080, select Yes, 4, Manual, Gasoline to get a result.

--- carfinder/my-app/src/index.js
+++ carfinder/my-app/src/index.js
@@ -3,89 +3,132 @@ import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap';
 import './index.css';
 
-var userInput = [null, null, null, null];
+var userInput = {
+  "tempFilter":null,
+  "doors":null,
+  "transmission":null,
+  "fueltype":null
+};
+
 const qna = [
   {
-    questionID: 0, 
-    question: "Would you like a car worth more than $50,000?", 
+    question: "Would you like a car worth more than $50,000?",
+    value: "tempFilter", 
     answer1: "Yes", 
-    answer1Value: "true",
+    answer1Value: true,
     answer2: "No",
-    answer2Value: "false"
+    answer2Value: false
   },
   {
-    questionID: 1, 
     question: "How many doors?", 
+    value: "doors",
     answer1: "2", 
-    answer1Value: "2",
+    answer1Value: 2,
     answer2: "4",
-    answer2Value: "4"
+    answer2Value: 4
   },
   {
-    questionID: 2, 
     question: "Automatic or manual?", 
+    value: "transmission",
     answer1: "Automatic", 
-    answer1Value: "automatic",
+    answer1Value: "Automatic",
     answer2: "Manual",
-    answer2Value: "manual"
+    answer2Value: "Manual"
   },
   {
-    questionID: 3, 
     question: "Electric or gasoline?", 
+    value: "fueltype",
     answer1: "Electric", 
-    answer1Value: "electric",
+    answer1Value: "Electric",
     answer2: "Gasoline",
-    answer2Value: "gasoline"
+    answer2Value: "Gas"
   }
 ];
 
 class App extends Component {
   state = {
-    isLoading: true,
-    isPressed: false,
-    groups: []
+    isLoading: false,
+    allButtonsClicked: false,
+    cars: []
   };
 
-  async componentDidMount() {
-    const response = await fetch('/cars');
+
+ async getCarsWithParam(){
+    this.setState({isLoading: true});
+    var firstEntry = true;
+    var requestString = '/cars/personalize';
+    
+    for (const property in userInput) {
+      if(userInput[property] == null){
+        continue;
+      }else{
+        if(!firstEntry){
+          requestString += '&';
+        }else{
+          requestString += '?';
+          firstEntry = false;
+        }
+        if(property == "tempFilter"){
+          if(userInput["tempFilter"] == true){
+            requestString += "minPrice=50000";
+          }else{
+            requestString += "maxPrice=50000";
+          }
+        }else{
+          requestString += property + "=" + userInput[property];
+        }
+      }
+    }
+
+    console.log(requestString);
+    const response = await fetch(requestString);
     const body = await response.json();
     console.log(body);
-    this.setState({ groups: body, isLoading: false });
+    this.setState({cars: body, isLoading: false})
   }
 
-  buttonPressed(index, value) {
-    const { isPressed } = this.state;
-    userInput[index] = value;
-    console.log(userInput);
+  buttonPressed(valueType, value) {
+    userInput[valueType] = value;
     this.forceUpdate();
-    if(userInput[0] != null && userInput[1] != null && userInput[2] != null && userInput[3] != null){
-        this.setState({isPressed:true});
+    var hasNull = false;
+    for (const property in userInput) {
+      if(userInput[property] == null){
+        hasNull = true;
+        break;
+      }else{
+        continue;
+      }
+    }
+
+    if(!hasNull){
+        this.setState({allButtonsClicked:true});
+        this.getCarsWithParam();
     }
   }
 
   render() {
-    const { groups, isPressed, isLoading } = this.state;
+    const { cars, allButtonsClicked, isLoading } = this.state;
 
     if (isLoading) {
       return <p>Loading...</p>;
     }
 
-    if (!isPressed) {
+    if (!allButtonsClicked) {
       return (
         <>
           <Header />
           {qna.map(questionandanswer =>{
-            if(userInput[questionandanswer.questionID] == null){
+            if(userInput[questionandanswer.value] == null){
             return <><div style={{ marginTop: 55, marginLeft: 10, marginRight: 10, marginBottom: 30, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', backgroundColor: "#F2ECB4", borderRadius: 22 }}>
             <h1>{questionandanswer.question}</h1>
           </div>
           <div style={{ padding: '0', textAlign: 'center' }}>
-            <Button style={{ marginTop: 5, marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.questionID, questionandanswer.answer1Value)}>{questionandanswer.answer1}</Button>
-            <Button style={{ marginTop: 5, marginLeft: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.questionID, questionandanswer.answer2Value)}>{questionandanswer.answer2}</Button>
-          </div></>
-          }
-        }
-          )
+            <Button style={{ marginTop: 5, marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.value, questionandanswer.answer1Value)}>{questionandanswer.answer1}</Button>
+            <Button style={{ marginTop: 5, marginLeft: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.value, questionandanswer.answer2Value)}>{questionandanswer.answer2}</Button>
+          </div>
+          </>
+              }
+            })
           }
 
         </>
@@ -98,8 +141,8 @@ class App extends Component {
           <h1>Here are some cars that match your needs!</h1>
         </div>
         <div style={{ textAlign: 'center' }}>
-          {userInput.map(input => {
-            return <p>{input}</p>
+          {cars.map(car => {
+            return <p>{car.make}, {car.model} a {car.transmission} car that runs on {car.fueltype} for ${car.price} with {car.doors} doors.</p>
           })
           }
         </div>
@@ -107,7 +150,6 @@ class App extends Component {
     }
   }
 }
-
 
 
 function Header() {
