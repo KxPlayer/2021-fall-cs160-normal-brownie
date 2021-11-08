3b6015c9cf0a5d10fd0a01bf97e44efedfa7b3a4
KxPlayer <69983255+KxPlayer@users.noreply.github.com>
2021-11-07 17:17:25 -0800

    Set up basic homepage that goes to quiz, sign up or log in "pages".
    Removed the auto build for the app (for testing), updated questions to allow for multiple answers, created a basic sign up section with a few conditions, set up a button to log in.

--- carfinder/my-app/src/index.js
+++ carfinder/my-app/src/index.js
@@ -13,35 +13,55 @@ var userInput = {
 const qna = [
   {
     question: "Would you like a car worth more than $50,000?",
-    value: "tempFilter", 
-    answer1: "Yes", 
-    answer1Value: true,
-    answer2: "No",
-    answer2Value: false
+    value: "tempFilter",
+    answers:[
+      {
+        answertext:"Yes",
+        value:true
+      },
+      {
+        answertext:"No",
+        value:false
+      }] 
   },
   {
     question: "How many doors?", 
     value: "doors",
-    answer1: "2", 
-    answer1Value: 2,
-    answer2: "4",
-    answer2Value: 4
+    answers:[
+      {
+        answertext:"2",
+        value:2
+      },
+      {
+        answertext:"4",
+        value:4
+      }]
   },
   {
     question: "Automatic or manual?", 
     value: "transmission",
-    answer1: "Automatic", 
-    answer1Value: "Automatic",
-    answer2: "Manual",
-    answer2Value: "Manual"
+    answers:[
+      {
+        answertext:"Automatic",
+        value:"Automatic"
+      },
+      {
+        answertext:"Manual",
+        value:"Manual"
+      }]
   },
   {
     question: "Electric or gasoline?", 
     value: "fueltype",
-    answer1: "Electric", 
-    answer1Value: "Electric",
-    answer2: "Gasoline",
-    answer2Value: "Gas"
+    answers:[
+      {
+        answertext:"Electric",
+        value:"Electric"
+      },
+      {
+        answertext:"Gasoline",
+        value:"Gas"
+      }]
   }
 ];
 
@@ -49,7 +69,13 @@ class App extends Component {
   state = {
     isLoading: false,
     allButtonsClicked: false,
-    cars: []
+    cars: [],
+    currentIndex: 0,
+    quizEntered: false,
+    signingUp: false,
+    loggingIn: false,
+    showUsernameLengthError: false,
+    showPasswordLengthError: false
   };
 
 
@@ -80,64 +106,205 @@ class App extends Component {
       }
     }
 
-    console.log(requestString);
     const response = await fetch(requestString);
     const body = await response.json();
-    console.log(body);
     this.setState({cars: body, isLoading: false})
   }
 
   buttonPressed(valueType, value) {
+    const {currentIndex, isLoading} = this.state;
     userInput[valueType] = value;
+    this.setState({currentIndex:currentIndex + 1})
     this.forceUpdate();
-    var hasNull = false;
+
+
+    if(currentIndex + 1 == qna.length){
+        this.setState({allButtonsClicked:true});
+        //this.getCarsWithParam();
+        this.setState({isLoading:false});
+
+        var firstEntry = true;
+    var requestString = '/cars/personalize';
+    
     for (const property in userInput) {
       if(userInput[property] == null){
-        hasNull = true;
-        break;
-      }else{
         continue;
+      }else{
+        if(!firstEntry){
+          requestString += '&';
+        }else{
+          requestString += '?';
+          firstEntry = false;
+        }
+        if(property == "tempFilter"){
+          if(userInput["tempFilter"] == true){
+            requestString += "minPrice=50000";
+          }else{
+            requestString += "maxPrice=50000";
+          }
+        }else{
+          requestString += property + "=" + userInput[property];
+        }
       }
     }
-
-    if(!hasNull){
-        this.setState({allButtonsClicked:true});
-        this.getCarsWithParam();
+    console.log(requestString);
     }
   }
 
+  previousQuestion(){
+    const{currentIndex} = this.state;
+    this.setState({currentIndex:currentIndex - 1});
+    if(currentIndex == qna.length){
+      this.setState({allButtonsClicked:false});
+      this.forceUpdate();
+    }
+  }
+
+  BackButton(){
+    const {currentIndex} = this.state;
+    if(currentIndex > 0){
+    return (<Button style={{backgroundColor: "#EB3E31", marginTop: 55, marginLeft: 20, marginRight: 10, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', borderWidth: 0}} onClick={() => this.previousQuestion()}>Back</Button>);
+    }
+    else{
+      return (<Button style={{backgroundColor: "#DBA29E", marginTop: 55, marginLeft: 20, marginRight: 10, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', color: '#636363', borderWidth: 0}}>Back</Button>);
+    }
+  }
+
+  SubmitUserSignUp(username, password){
+    console.log(username, password);
+    var validSignup = true;
+    if(password.length < 8){
+      console.log("password too short")
+      this.setState({showPasswordLengthError:true});
+      validSignup = false;
+    } else{
+      this.setState({showPasswordLengthError:false});
+    }
+    if(username.length == 0){
+      console.log("need to input a username")
+      this.setState({showUsernameLengthError:true});
+      validSignup = false;
+    }else{
+      this.setState({showUsernameLengthError:false});
+    }
+    if(validSignup){
+      this.setState({signingUp:false});
+    }
+    // submit here?
+  }
+
   render() {
-    const { cars, allButtonsClicked, isLoading } = this.state;
+    const { cars, allButtonsClicked, isLoading, currentIndex, quizEntered, signingUp, loggingIn, showPasswordLengthError, showUsernameLengthError} = this.state;
 
     if (isLoading) {
       return <p>Loading...</p>;
     }
+    if(!quizEntered && !signingUp && !loggingIn){
+      return(<>
+      <Header /><div style={{ marginTop: 55, textAlign: 'center'}}>
+      <h1>Take a quiz to find a car that matches your needs!</h1>
+      <p>If you find something you like, log in to save it to your favorites list.</p>
+      <p>If you don't have an account, sign up!</p>
+      </div>
+      <div style={{textAlign: 'center'}}>
+      <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={()=>this.setState({quizEntered:true})}>Take quiz</Button>
+      <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={()=>this.setState({signingUp:true})}>Sign up</Button>
+      <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={()=>this.setState({loggingIn:true})}>Log in</Button></div></>);
+    
+  }else if(signingUp){
+      if (showPasswordLengthError && showUsernameLengthError){
+        return (<><Header />
+          <div style={{ marginTop: 55, marginLeft: '30%'}}>
+          <form>
+            <label>
+              Username: <input type="text" name="username" id="username" required/>
+            </label>
+            <br></br>
+            <label>
+              Password: <input type="password" name="password" id="password" minLength="8" required />
+            </label>
+            <br></br>
+            <Button onClick={()=> this.SubmitUserSignUp(document.getElementById("username").value, document.getElementById("password").value)}>Submit</Button>
+          </form>
+          
+        <p>You need to enter a username.</p>
+        <p>The password needs to be at least 8 characters.</p></div>
+        </>);
+      }else if (showPasswordLengthError){
+        return (<><Header />
+          <div style={{ marginTop: 55, marginLeft: '30%'}}>
+          <form>
+            <label>
+              Username: <input type="text" name="username" id="username" required/>
+            </label>
+            <br></br>
+            <label>
+              Password: <input type="password" name="password" id="password" minLength="8" required />
+            </label>
+            <br></br>
+            <Button onClick={()=> this.SubmitUserSignUp(document.getElementById("username").value, document.getElementById("password").value)}>Submit</Button>
+          </form>
+          
+          <p>The password needs to be at least 8 characters.</p></div>
+          </>);
+      }else if (showUsernameLengthError){
+      return (<><Header />
+        <div style={{ marginTop: 55, marginLeft: '30%'}}>
+        <form>
+          <label>
+            Username: <input type="text" name="username" id="username" required/>
+          </label>
+          <br></br>
+          <label>
+            Password: <input type="password" name="password" id="password" minLength="8" required />
+          </label>
+          <br></br>
+          <Button onClick={()=> this.SubmitUserSignUp(document.getElementById("username").value, document.getElementById("password").value)}>Submit</Button>
+        </form>
+        <p>You need to enter a username.</p></div></>
+        );
+    }else{
+      return (<><Header />
+      <div style={{ marginTop: 55, marginLeft: '30%'}}>
+      <form>
+        <label>
+          Username: <input type="text" name="username" id="username" required/>
+        </label>
+        <br></br>
+        <label>
+          Password: <input type="password" name="password" id="password" minLength="8" required />
+        </label>
+        <br></br>
+        <Button onClick={()=> this.SubmitUserSignUp(document.getElementById("username").value, document.getElementById("password").value)}>Submit</Button>
+      </form>
+      </div>
+      </>);
+    }
 
+    }else if(quizEntered){
     if (!allButtonsClicked) {
+      var questionandanswer = qna[currentIndex];
       return (
         <>
           <Header />
-          {qna.map(questionandanswer =>{
-            if(userInput[questionandanswer.value] == null){
-            return <><div style={{ marginTop: 55, marginLeft: 10, marginRight: 10, marginBottom: 30, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', backgroundColor: "#F2ECB4", borderRadius: 22 }}>
+          {this.BackButton()}
+            <div style={{ marginTop: 10, marginLeft: 10, marginRight: 10, marginBottom: 30, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', backgroundColor: "#F2ECB4", borderRadius: 22}}>
             <h1>{questionandanswer.question}</h1>
           </div>
-          <div style={{ padding: '0', textAlign: 'center' }}>
-            <Button style={{ marginTop: 5, marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.value, questionandanswer.answer1Value)}>{questionandanswer.answer1}</Button>
-            <Button style={{ marginTop: 5, marginLeft: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.value, questionandanswer.answer2Value)}>{questionandanswer.answer2}</Button>
-          </div>
-          </>
-              }
-            })
-          }
-
+          
+          <div style={{ padding: '0', textAlign: 'center', marginTop: 10 }}>
+            {questionandanswer.answers.map(answerbuttons =>{
+              return <Button style={{ marginTop: 0, marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.value, answerbuttons.value)}>{answerbuttons.answertext}</Button>
+            })}
+          </div>    
         </>
 
       )
     } else {
       return <>
         <Header />
-        <div style={{ marginTop: 55, marginLeft: 10, marginRight: 10, marginBottom: 30, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', backgroundColor: "#C2F2BA", borderRadius: 22 }}>
+        {this.BackButton()}
+        <div style={{ marginTop: 10, marginLeft: 10, marginRight: 10, marginBottom: 30, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', backgroundColor: "#C2F2BA", borderRadius: 22 }}>
           <h1>Here are some cars that match your needs!</h1>
         </div>
         <div style={{ textAlign: 'center' }}>
@@ -148,9 +315,13 @@ class App extends Component {
         </div>
       </>
     }
+  } else if(loggingIn){
+    return <>
+    <Header/>
+    </>
+  }
   }
 }
-
 
 function Header() {
   return (
