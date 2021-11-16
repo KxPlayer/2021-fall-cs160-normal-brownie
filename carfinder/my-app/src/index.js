bd895bf6b0438ca94709dee7f7f6744b8c477913
KxPlayer <69983255+KxPlayer@users.noreply.github.com>
2021-11-16 14:40:22 -0800

    Added test java file for front end with Selenium, updated page with new questions and log in checks the database (but doesn't save it)
    Testing only works for chrome and you need to specify the location of the Chrome driver on your own. You need to take the code out of FrontEndTesting.java, build the project in cmd line (mvnw spring-boot:run) then put the code back in and run it.

--- carfinder/my-app/src/index.js
+++ carfinder/my-app/src/index.js
@@ -3,29 +3,46 @@ import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap';
 import './index.css';
 
-var userInput = {
-  "tempFilter":null,
-  "doors":null,
-  "transmission":null,
-  "fueltype":null
-};
+var userInput = {};
 
 const qna = [
   {
-    question: "Would you like a car worth more than $50,000?",
-    value: "tempFilter",
+    question: "What is your price range?",
+    value: "priceRange",
     answers:[
       {
-        answertext:"Yes",
-        value:true
+        answertext:"$0 to $7500",
+        value:[0, 7500]
       },
       {
-        answertext:"No",
-        value:false
+        answertext:"$7500 to $35000",
+        value:[7500, 35000]
+      },
+      {
+        answertext:"$35000 to $100000",
+        value:[35000, 100000]
       }] 
   },
   {
-    question: "How many doors?", 
+    question: "What range of years would you like the car?",
+    value: "yearRange",
+    answers:[
+      {
+        answertext:"1900-1950",
+        value:[1900, 1950]
+      },
+      {
+        answertext:"1950-2000",
+        value:[1950, 2000]
+      },
+      {
+        answertext:"2000-2022",
+        value:[2000, 2022]
+      }
+    ]
+  },
+  {
+    question: "How many doors would you like?", 
     value: "doors",
     answers:[
       {
@@ -38,7 +55,7 @@ const qna = [
       }]
   },
   {
-    question: "Automatic or manual?", 
+    question: "Would you like an automatic or manual car?", 
     value: "transmission",
     answers:[
       {
@@ -51,7 +68,7 @@ const qna = [
       }]
   },
   {
-    question: "Electric or gasoline?", 
+    question: "Would you like an electric or gasoline car?", 
     value: "fueltype",
     answers:[
       {
@@ -62,6 +79,28 @@ const qna = [
         answertext:"Gasoline",
         value:"Gas"
       }]
+  },
+  {
+    question: "Please select the minimum amount of mileage you would like.",
+    value: "minmpgcombined",
+    answers:[
+      {
+        answertext:"Any/Electric",
+        value: null
+      },
+      {
+        answertext:"20+",
+        value: 20
+      },
+      {
+        answertext:"30+",
+        value: 30
+      },
+      {
+        answertext:"40+",
+        value: 40
+      }
+    ]
   }
 ];
 
@@ -75,7 +114,8 @@ class App extends Component {
     signingUp: false,
     loggingIn: false,
     showUsernameLengthError: false,
-    showPasswordLengthError: false
+    showPasswordLengthError: false,
+    loggedIn: false
   };
 
 
@@ -94,18 +134,19 @@ class App extends Component {
           requestString += '?';
           firstEntry = false;
         }
-        if(property === "tempFilter"){
-          if(userInput["tempFilter"] === true){
-            requestString += "minPrice=50000";
-          }else{
-            requestString += "maxPrice=50000";
-          }
+        if(property === "priceRange"){
+          requestString += "minPrice=" + userInput["priceRange"][0];
+          requestString += "&maxPrice=" + userInput["priceRange"][1];
+        }else if(property === "yearRange"){
+          requestString += "minYear=" + userInput["yearRange"][0];
+          requestString += "&maxYear=" + userInput["yearRange"][1];
         }else{
           requestString += property + "=" + userInput[property];
         }
       }
     }
 
+    console.log(requestString);
     const response = await fetch(requestString);
     const body = await response.json();
     this.setState({cars: body, isLoading: false})
@@ -117,37 +158,10 @@ class App extends Component {
     this.setState({currentIndex:currentIndex + 1})
     this.forceUpdate();
 
-
     if(currentIndex + 1 === qna.length){
         this.setState({allButtonsClicked:true});
         this.getCarsWithParam();
         this.setState({isLoading:false});
-
-    var firstEntry = true;
-    var requestString = '/cars/personalize';
-    
-    for (const property in userInput) {
-      if(userInput[property] === null){
-        continue;
-      }else{
-        if(!firstEntry){
-          requestString += '&';
-        }else{
-          requestString += '?';
-          firstEntry = false;
-        }
-        if(property === "tempFilter"){
-          if(userInput["tempFilter"] === true){
-            requestString += "minPrice=50000";
-          }else{
-            requestString += "maxPrice=50000";
-          }
-        }else{
-          requestString += property + "=" + userInput[property];
-        }
-      }
-    }
-    console.log(requestString);
     }
   }
 
@@ -197,15 +211,23 @@ class App extends Component {
     }
   }
 
-  SubmitUserLogIn(username, password){
-    console.log(username, password);
-    this.setState({loggingIn:false, quizEntered:true})
+  async SubmitUserLogIn(username, password){
+    var requestString = '/users/username';
+    requestString += '?username=' + username;
+    const response = await fetch(requestString);
+    var json = await response.json().catch(err => {console.log("username not found")});
+    if (json != undefined){
+      if (password == json["password"]){
+        this.setState({loggingIn:false, loggedIn:true, quizEntered:true});
+      }
+    }
   }
   
   render() {
     const { cars, allButtonsClicked, isLoading, currentIndex, quizEntered, signingUp, loggingIn, showPasswordLengthError, showUsernameLengthError} = this.state;
     
     if (isLoading) {
+      
       return <p>Loading...</p>;
     }
     if(!quizEntered && !signingUp && !loggingIn){
@@ -216,14 +238,15 @@ class App extends Component {
         <p>If you don't have an account, sign up!</p>
         </div>
         <div style={{textAlign: 'center'}}>
-        <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={()=>this.setState({quizEntered:true})}>Take quiz</Button>
-        <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={()=>this.setState({signingUp:true})}>Sign up</Button>
-        <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={()=>this.setState({loggingIn:true})}>Log in</Button></div>
+        <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} name="quizButton" onClick={()=>this.setState({quizEntered:true})}>Take quiz</Button>
+        <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} name="signUpButton" onClick={()=>this.setState({signingUp:true})}>Sign up</Button>
+        <Button style={{ marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} name="logInButton" onClick={()=>this.setState({loggingIn:true})}>Log in</Button></div>
         </>);
     
   }else if(signingUp){
       return <>{<><Header />
         <div style={{ marginTop: 55, marginLeft: '30%'}}>
+        
         <form>
           <label>
             Username: <input type="text" name="username" id="username" required/>
@@ -233,11 +256,13 @@ class App extends Component {
             Password: <input type="password" name="password" id="password" minLength="8" required />
           </label>
           <br></br>
-          <Button onClick={()=> this.SubmitUserSignUp(document.getElementById("username").value, document.getElementById("password").value)}>Submit</Button>
+          <Button onClick={()=> this.SubmitUserSignUp(document.getElementById("username").value, document.getElementById("password").value)} id="submitSignup">Submit</Button>
         </form>
-        {showUsernameLengthError ? (<p>You need to enter a username.</p>):(<></>)}
-        {showPasswordLengthError ? (<p>The password needs to be at least 8 characters.</p>):(<></>)}
+        {showUsernameLengthError ? (<p id="usernameLengthError">You need to enter a username.</p>):(<></>)}
+        {showPasswordLengthError ? (<p id="passwordLengthError">The password needs to be at least 8 characters.</p>):(<></>)}
+        <Button onClick={()=> this.setState({signingUp:false})} id="exitSignUp">Exit</Button>
         </div></>}
+        
       </>
 
     }else if(quizEntered){
@@ -253,7 +278,7 @@ class App extends Component {
           
           <div style={{ padding: '0', textAlign: 'center', marginTop: 10 }}>
             {questionandanswer.answers.map(answerbuttons =>{
-              return <Button style={{ marginTop: 0, marginRight: 15, width: 200, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 30 }} onClick={() => this.buttonPressed(questionandanswer.value, answerbuttons.value)}>{answerbuttons.answertext}</Button>
+              return <Button style={{ marginTop: 0, marginRight: 15, width: 250, height: 200, borderWidth: 0, borderRadius: 13, backgroundColor: "#C6EBE9", fontSize: 25 }} id={questionandanswer.value + answerbuttons.value} onClick={() => {this.buttonPressed(questionandanswer.value, answerbuttons.value); console.log(questionandanswer.value + answerbuttons.value);}}>{answerbuttons.answertext}</Button>
             })}
           </div>    
         </>
@@ -266,9 +291,9 @@ class App extends Component {
         <div style={{ marginTop: 10, marginLeft: 10, marginRight: 10, marginBottom: 30, paddingLeft: '5%', paddingRight: '5%', paddingTop: '1%', paddingBottom: '1%', textAlign: 'center', backgroundColor: "#C2F2BA", borderRadius: 22 }}>
           <h1>Here are some cars that match your needs!</h1>
         </div>
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center' }} id="results">
           {cars.map(car => {
-            return <p>{car.make}, {car.model} a {car.transmission} car that runs on {car.fueltype} for ${car.price} with {car.doors} doors.</p>
+            return <p class="result">{car.make}, {car.model} a {car.transmission} car that runs on {car.fueltype} for ${car.price} with {car.doors} doors.</p>
           })
           }
         </div>
@@ -277,6 +302,7 @@ class App extends Component {
   } else if(loggingIn){
     return <>{<><Header />
         <div style={{ marginTop: 55, marginLeft: '30%'}}>
+          
         <form>
           <label>
             Username: <input type="text" name="username" id="username" required/>
@@ -286,9 +312,11 @@ class App extends Component {
             Password: <input type="password" name="password" id="password" minLength="8" required />
           </label>
           <br></br>
-          <Button onClick={()=> this.SubmitUserLogIn(document.getElementById("username").value, document.getElementById("password").value)}>Submit</Button>
+          <Button onClick={()=> this.SubmitUserLogIn(document.getElementById("username").value, document.getElementById("password").value)} id="submitLogin">Submit</Button>
         </form>
+        <Button onClick={()=> this.setState({loggingIn:false})} id="exitLogIn">Exit</Button>
         </div></>}
+        
       </>
   }
   }
